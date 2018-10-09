1124c88a40cac2f521d3d7887cf90fd32fbeacca
YveLai <43787419+YveLai@users.noreply.github.com>
2018-10-09 02:43:53 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
   return 'Hello Napier'
+
+@app.route("/no")
+def lonely():
+   return 'no lover'
